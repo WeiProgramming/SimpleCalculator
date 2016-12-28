7d98a1418ab590ece2452112d8b7f6f8da32e399
WeiProgramming <wei.leung1995@gmail.com>
2016-12-28 00:46:03 -0800

    Added add, subtract,divide,multiply, and remainder methods

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,5 +1,24 @@
 package Calculator;
 
 public class Calculator {
+	public Calculator(){
+		
+	}
+	
+	public Double add(){
+		return null;
+	}
+	public Double subract(){
+		return null;
+	}
+	public Double multiply(){
+		return null;
+	}
+	public Double divide(){
+		return null;
+	}
+	public Double remainder(){
+		return null;
+	}
 
 }
