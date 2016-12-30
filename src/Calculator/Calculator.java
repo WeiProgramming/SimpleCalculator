e20cb9efae228d629ba4695a8ada161e82ef1109
WeiProgramming <wei.leung1995@gmail.com>
2016-12-30 11:25:04 -0800

    Modified calc and calcapp, can't seem to make the calulator work

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,27 +1,57 @@
 package Calculator;
 
 public class Calculator {
-	Double a,b;
-	String eq;
+	private Double solution;
+	private String add = "+";
+	private String sub = "-";
+	private String mult = "*";
+	private String mod = "%";
+	private String div = "/";
+
 	
-	public Calculator(Double a, Double b, String eq){
-		this.a = a;
-		this.b = b;
-		this.eq = eq;
+	
+	public Calculator(){
 	}
-	public Double add(){
-		return (double) (a + b);
+	
+	public void add(Double a, Double b){
+		solution = (double) (a + b);
+		System.out.println("The answer is: "+ solution);
 	}
-	public Double subract(){
-		return (double) (a - b);
+	public void subract(Double a, Double b){
+		solution = (double) (a - b);
+		System.out.println("The answer is: "+ solution);
 	}
-	public Double multiply(){
-		return (double) (a * b);
+	public void multiply(Double a, Double b){
+		solution = (double) (a * b);
+		System.out.println("The answer is: "+ solution);
 	}
-	public Double divide(){
-		return (double) (a/b);
+	public void divide(Double a, Double b){
+		solution = (double) (a/b);
+		System.out.println("The answer is: "+ solution);
 	}
-	public Double remainder(){
-		return (double) (a%b);
+	public void remainder(Double a, Double b){
+		solution = (double) (a%b);
+		System.out.println("The answer is: "+ solution);
+	}
+	public void solution(Double a, Double b,String operand){
+		String eq = operand;
+		if(eq == add){
+			add(a, b);
+		}
+		else if(operand == sub){
+			subract(a, b);
+		}
+		else if(operand == mult){
+			multiply(a, b);
+		}
+		else if (operand == div){
+			divide(a, b);
+		}
+		else if (operand == mod){
+			remainder(a, b);
+		}
+		else{
+			System.out.println("Invalid");
+		}
 	}
 }
--- src/Test/CalculatorApp.java
+++ src/Test/CalculatorApp.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 import Calculator.*;
 
 public class CalculatorApp{
+	private static Double solution;
 	
 	public static void main(String[] args){
 		System.out.println("Welcome To Simple Calculator\n");
@@ -14,27 +15,15 @@ public class CalculatorApp{
 		
 		System.out.println("Please Enter Operand");
 		String operand = scanner.next();
+		operand.toString();
 		
 		System.out.println("Please Enter Second Number");
 		Double secondNum = scanner.nextDouble();
+		scanner.close();
 
-		Calculator calculator = new Calculator(firstNum, secondNum, operand);
+		Calculator calculator = new Calculator();
+		calculator.solution(firstNum,secondNum,operand);
 		
-		if(operand == "+"){
-//			calculator.add();
-			System.out.println("Your answer is " + calculator.add());
-		}
-		else if(operand == "-"){
-			calculator.subract();
-		}
-		else if(operand == "*"){
-			calculator.multiply();
-		}
-		else if (operand == "/"){
-			calculator.divide();
-		}
-		else if (operand == "%"){
-			calculator.remainder();
-		}
+//		System.out.println("first"+firstNum+"second"+secondNum+"operand"+operand+"sol"+solution);
 	}
 }
